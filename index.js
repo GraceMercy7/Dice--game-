9ec0e1aaf7f33ec56e76dbfc87a4436287ae7495
GraceMercy7 <eyoabaddise99@gmail.com>
2024-11-21 11:36:19 +0300

    dicee-game  index.js

---
+++ index.js
@@ -0,0 +1,7 @@
+var randomNumber1=Math.floor(Math.random() * 6) + 1 ;
+var randomimage = "dice " + randomnumber1 + ".png";
+var randomlk = "./image" + randomimage;
+ var image  = document.querySelectorAll("img")[0];
+image.setAttribute("src" , randommlk);
+var random2 = Math.floor(Math.random() * 6 ) + 1 ;
+var ranodmsoruce = "./image/dice " + randomnumber1 + ".png";
